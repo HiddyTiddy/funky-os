9cd935ca3bca1a8d5018838386af7361fc807bf3
HiddyTiddy <hiddy.tiddey@gmail.com>
2022-02-05 11:45:56 +0100

    restructured stuff

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,18 @@
 // nightly stuff
 #![feature(custom_test_frameworks)]
 #![feature(abi_x86_interrupt)]
-#![test_runner(crate::test_runner)]
+#![test_runner(os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
 
 use core::panic::PanicInfo;
-use os::utils::str_to_int;
-use os::{eprintln, println, interrupts};
+use os::ports::{port_byte_in, port_byte_out};
+use os::{eprintln, interrupts, println, serial_println};
+
 
 // use crate::ports::tst;
 #[panic_handler]
@@ -24,25 +23,39 @@ fn panic(info: &PanicInfo) -> ! {
     loop { /* unrecoverable panic attack */ }
 }
 
+
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // init
     interrupts::init_idt();
-    x86_64::instructions::interrupts::int3();
+    //x86_64::instructions::interrupts::int3();
 
-    println!("hi");
-    println!("!!!");
-    //tst();
-    println!("{}", str_to_int(&[b'1', b'0', b'5']));
-    println!("{}", str_to_int(&[b'0', b'0', b'5']));
+    // println!("hi");
+    // println!("!!!");
+    // //tst();
+    // println!("{}", str_to_int(&[b'1', b'0', b'5']));
+    // println!("{}", str_to_int(&[b'0', b'0', b'5']));
 
     fn lol_die() {
         lol_die();
     }
-    lol_die();
+    //lol_die();
+    println!("lol");
+    serial_println!("hello to host");
+    // port_byte_out(port+3, 0x80);
 
-    os::color_test();
-    println!("still alive");
+    // port_byte_out(port  , 0x03);
+    // port_byte_out(port+1, 0x00);
+    // port_byte_out(port+3, 0x03);
+    // port_byte_out(port+2, 0xc7);
+    // port_byte_out(port+4, 0x0b);
+    // port_byte_out(port+1, 0x01);
+
+    // port_byte_out(port, b'h');
+
+    // os::color_test();
+    // println!("still alive");
 
     // video_tmp()
 
