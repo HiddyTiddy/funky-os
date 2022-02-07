124c73d5eab757286bab457d0069d813ba5af8ff
HiddyTiddy <hiddy.tiddey@gmail.com>
2022-02-07 14:48:27 +0100

    struct syntax for ports

--- src/ports.rs
+++ src/ports.rs
@@ -2,6 +2,22 @@ use core::arch::asm;
 
 use crate::println;
 
+pub struct Port {
+    port_num: u16,
+}
+
+impl Port {
+    pub fn write(&mut self, data: u8) {
+            port_byte_out(self.port_num, data);
+    }
+    pub fn read(&self) -> u8 {
+        port_byte_in(self.port_num)
+    }
+
+    pub const fn new(port: u16) -> Self {
+        Port { port_num: port }
+    }
+}
 pub fn port_byte_in(port: u16) -> u8 {
     let out: u8;
     unsafe { asm!("in al, dx", out("al") out, in("dx") port) }
@@ -12,15 +28,15 @@ pub fn port_byte_out(port: u16, data: u8) {
     unsafe { asm!("out dx, al", in("al")data, in("edx")port) }
 }
 
-fn port_word_in(port: u16) -> u16 {
-    let out: u16;
-    unsafe { asm!("in ax, dx", out("ax")out, in("dx")port) }
-    out
-}
-
-fn port_word_out(port: u16, data: u16) {
-    unsafe { asm!("out dx, ax", in("ax")data, in("edx")port) }
-}
+// fn port_word_in(port: u16) -> u16 {
+//     let out: u16;
+//     unsafe { asm!("in ax, dx", out("ax")out, in("dx")port) }
+//     out
+// }
+// 
+// fn port_word_out(port: u16, data: u16) {
+//     unsafe { asm!("out dx, ax", in("ax")data, in("edx")port) }
+// }
 
 // https://stanislavs.org/helppc/ports.html
 
@@ -32,7 +48,6 @@ pub fn tst() {
     port_byte_out(0x3d4, 15);
     pos += port_byte_in(0x3d5) as u16;
     println!("0x{:x}", pos);
-
 
     println!("graphics mode: 0x{:x}", port_byte_in(0x3ce));
 }
