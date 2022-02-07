fd9b8a937a0e7a501b796d4e8d7697e6c7a065de
HiddyTiddy <hiddy.tiddey@gmail.com>
2022-02-07 14:48:44 +0100

    get clippy to shut up

--- src/serial.rs
+++ src/serial.rs
@@ -1,4 +1,4 @@
-use core::fmt::{Write, Arguments};
+use core::fmt::{Arguments, Write};
 
 use crate::ports::{port_byte_in, port_byte_out};
 use lazy_static::lazy_static;
@@ -21,7 +21,7 @@ impl SerialPort {
         SerialPort { port }
     }
     fn is_transmit_empty(&self) -> u8 {
-        return port_byte_in(self.port + 5) & 0x20;
+        port_byte_in(self.port + 5) & 0x20
     }
 
     fn write_byte(&self, a: u8) {
@@ -33,23 +33,23 @@ impl SerialPort {
         // adapted from https://wiki.osdev.org/Serial_Ports
         port_byte_out(self.port + 1, 0x00); // Disable all interrupts
         port_byte_out(self.port + 3, 0x80); // Enable DLAB (set baud rate divisor)
-        port_byte_out(self.port + 0, 0x03); // Set divisor to 3 (lo byte) 38400 baud
+        port_byte_out(self.port , 0x03); // Set divisor to 3 (lo byte) 38400 baud
         port_byte_out(self.port + 1, 0x00); //                  (hi byte)
         port_byte_out(self.port + 3, 0x03); // 8 bits, no parity, one stop bit
         port_byte_out(self.port + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
         port_byte_out(self.port + 4, 0x0B); // IRQs enabled, RTS/DSR set
         port_byte_out(self.port + 4, 0x1E); // Set in loopback mode, test the serial chip
-        port_byte_out(self.port + 0, 0xAE); // Test serial chip (send byte 0xAE and check if serial returns same byte)
+        port_byte_out(self.port , 0xAE); // Test serial chip (send byte 0xAE and check if serial returns same byte)
 
         // Check if serial is faulty (i.e: not same byte as sent)
-        if port_byte_in(self.port + 0) != 0xAE {
-            return Err(());
+        if port_byte_in(self.port ) != 0xAE {
+            Err(())
+        } else {
+            // If serial is not faulty set it in normal operation mode
+            // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
+            port_byte_out(self.port + 4, 0x0F);
+            Ok(())
         }
-
-        // If serial is not faulty set it in normal operation mode
-        // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
-        port_byte_out(self.port + 4, 0x0F);
-        return Ok(());
     }
 }
 
@@ -62,10 +62,11 @@ impl Write for SerialPort {
     }
 }
 
-
 #[doc(hidden)]
 pub fn _serial_print(args: Arguments) {
-    SERIAL_COM1.lock().write_fmt(args).unwrap();
+    x86_64::instructions::interrupts::without_interrupts(|| {
+        SERIAL_COM1.lock().write_fmt(args).unwrap();
+    });
 }
 
 #[macro_export]
@@ -78,4 +79,3 @@ macro_rules! serial_println {
     () => ($crate::serial_print!("\n"));
     ($($arg:tt)*) => ($crate::serial_print!("{}\n", format_args!($($arg)*)));
 }
-
